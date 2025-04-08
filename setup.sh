3d18e1ac4d36b7c867d13ea819fb358d9291c4fa
Brandon Corn <brandond.corn@gmail.com>
2025-04-08 10:56:16 -0700

    Add display option for inky_phat

--- setup.sh
+++ setup.sh
@@ -56,10 +56,18 @@ fi
 echo
 echo "###### Display setup"
 PS3="Please select your Display Model: "
-options=("Pimoroni Inky Impression 4 (640x400)" "Waveshare 4.01inch ACeP 4 (640x400)" "Pimoroni Inky Impression 5.7 (600x448)" "Pimoroni Inky Impression 7.3 (800x480)")
+options=("Pimoroni Inky pHAT 2.13 (212x104)" "Pimoroni Inky Impression 4 (640x400)" "Waveshare 4.01inch ACeP 4 (640x400)" "Pimoroni Inky Impression 5.7 (600x448)" "Pimoroni Inky Impression 7.3 (800x480)")
 select opt in "${options[@]}"
 do
     case $opt in
+        "Pimoroni Inky Impression 4 (640x400)")
+            echo "[DEFAULT]" >> ${install_path}/config/eink_options.ini
+            echo "width = 212" >> ${install_path}/config/eink_options.ini
+            echo "height = 104" >> ${install_path}/config/eink_options.ini
+            echo "album_cover_small_px = 0" >> ${install_path}/config/eink_options.ini
+            echo "model = inky-phat" >> ${install_path}/config/eink_options.ini
+            break
+            ;;
         "Pimoroni Inky Impression 4 (640x400)")
             echo "[DEFAULT]" >> ${install_path}/config/eink_options.ini
             echo "width = 640" >> ${install_path}/config/eink_options.ini
